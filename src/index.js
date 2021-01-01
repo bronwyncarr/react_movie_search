aefd482dac8fc56ac60ebd03be15dba4be05908e
bronwyncarr <bronwynacarr@gmail.com>
2021-01-01 21:20:46 +1100

    formatting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // API Key: e201a6a56670b97f3a5f97c76b8c85e2
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
